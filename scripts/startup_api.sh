5075f6546d6fc7a3f2e63c7caafc83bb37420ab5
v3nd3774 <josue.caraballo@gmail.com>
2020-04-05 11:21:48 -0500

    add startup api

---
+++ scripts/startup_api.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# V1
+# March 29, 2020
+# written by Josue Caraballo (josue.caraballo@mavs.uta.edu)
+# This script should be run as a user.
+# It:
+#  1) cds to the right dir
+#  2) runs the api
+cd api
+python3 app.py
+
