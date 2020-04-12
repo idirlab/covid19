6096ddedfc3e4c4d481a599393bcee89982a6bd8
v3nd3774 <josue.caraballo@gmail.com>
2020-04-11 22:21:07 -0500

    again with typos

--- scripts/swap-api-url-lh.sh
+++ scripts/swap-api-url-lh.sh
@@ -5,4 +5,4 @@
 # This script should be run as a user.
 # It:
 #  1) swaps the url from the one running on 2222 to the one running on 3333
-perl -pi -E "s/https\:\/\/idir\.uta\.edu/http\:\/\localhost\:3333/g" js/interactive.js
+perl -pi -E "s/https\:\/\/idir\.uta\.edu/http\:\/\/localhost\:3333/g" js/interactive.js
