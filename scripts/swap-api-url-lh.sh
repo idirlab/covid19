fea631f3f81ceeca3a641ad57f9ea4e0f3b39ce0
v3nd3774 <josue.caraballo@gmail.com>
2020-04-11 21:34:04 -0500

    storing change to port

--- scripts/swap-api-url-lh.sh
+++ scripts/swap-api-url-lh.sh
@@ -5,4 +5,4 @@
 # This script should be run as a user.
 # It:
 #  1) swaps the url from the one running on 2222 to the one running on 3333
-perl -pi -E "s/https\:\/\/idir\.uta\.edu\/covid-19-api-dev/localhost\:2222/g" js/interactive.js
+perl -pi -E "s/https\:\/\/idir\.uta\.edu\/covid-19-api-dev/localhost\:3333/g" js/interactive.js
