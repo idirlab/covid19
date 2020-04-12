a94ea714fb527607ccb9efb42ebfdff6fb2774fd
v3nd3774 <josue.caraballo@gmail.com>
2020-04-11 21:31:26 -0500

    storing lh changes

---
+++ scripts/swap-api-url-lh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# V1
+# April 5th, 2020
+# written by Josue Caraballo (josue.caraballo@mavs.uta.edu)
+# This script should be run as a user.
+# It:
+#  1) swaps the url from the one running on 2222 to the one running on 3333
+perl -pi -E "s/https\:\/\/idir\.uta\.edu\/covid-19-api-dev/localhost\:2222/g" js/interactive.js
