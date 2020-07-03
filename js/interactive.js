b09665f53d2f8e8715ed9adb2a5c3c6e44ed149a
824zzy <614594368@qq.com>
2020-07-03 18:10:28 -0500

    fix($loc): change url for tile layer and remove redundant url

--- js/interactive.js
+++ js/interactive.js
@@ -201,7 +201,7 @@ $(document).ready(async function(){
     position: 'bottomright'
   }).addTo(mymap);
 
-  L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{ maxZoom: 20, subdomains:['mt0','mt1','mt2','mt3']
+  L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{ maxZoom: 20, subdomains:['mt0','mt1','mt2','mt3']
   }).addTo(mymap);
 
   var circle_group;
